f25a9d479013e42a0d671a472799aac8a0e12c4e
João Vitor <97707591+jvdsantos3@users.noreply.github.com>
2022-05-24 09:22:13 -0300

    Add files via upload

--- av2.py
+++ av2.py
@@ -1,3 +1,7 @@
+def format_telefone(tel):
+    telefone = "({}) {}-{}".format(tel[0] + tel[1], tel[2] + tel[3] + tel[4] + tel[5] + tel[6],tel[7] + tel[8] + tel[9] + tel[10])
+    return telefone
+
 clientes = {}
 
 print("Menu")
@@ -6,44 +10,52 @@ decisao = int(input("\nEscolha uma opção: "))
 
 while (decisao != 5):
     if (decisao == 1):
-        cpf = str(input("Digite o CPF do cliente: "))
+        email = str(input("\nDigite o E-mail do cliente: "))
         nome = str(input("Digite o nome do cliente: "))
-        telefone = str(input("Digite o telefone do cliente: "))
+        nome = nome.capitalize()
+        telefone_nao_formatado = input("Digite o telefone do cliente: ")
+        telefone = format_telefone(telefone_nao_formatado)
 
-        clientes.update({cpf: {'nome': nome, 'telefone': telefone}})
+        clientes.update({email: {'nome': nome, 'telefone': telefone}})
 
     elif (decisao == 2):
-        cpf = input("\nDigite o CPF do cliente: ")
-        print(f"\n{clientes.get(cpf, 'CPF não encontrado.')}")
+        email = input("\nDigite o E-mail do cliente: ")
+        if (email in clientes):
+            print(f"Nome: {clientes[email]['nome']}\nTelefone: {clientes[email]['telefone']}")
+        else:
+            print("E-mail não encontrado.")
 
     elif (decisao == 3):
-        cpf = str(input("\nDigite o CPF do cliente: "))
-        resultado = clientes.get(cpf, False)
-        if resultado:
-            escolha = int(input("ALterar\n1 - Nome\n2 - Telefone\n3 - Sair\nEscolha uma das opções: "))
-            if (escolha == 1):
-                novoNome = input("Digite o novo nome do cliente: ")
-                clientes[cpf].update({'nome': novoNome})
-                print('Nome atualizado.')
-            elif (escolha == 2):
-                novoTelefone = input("Digite o novo telefone do cliente: ")
-                clientes[cpf].update({'telefone': novoTelefone})
-                print("Telefone atualizado")
-            elif (escolha == 3):
-                continue
+        email = str(input("\nDigite o E-mail do cliente: "))
+        resultado = clientes.get(email, False)
+        escolha = 1
+
+        while (escolha != 3):
+            if resultado:
+                escolha = int(input("\nAlterar\n\n1 - Nome\n2 - Telefone\n3 - Sair\n\nEscolha uma das opções: "))
+                if (escolha == 1):
+                    novoNome = input("\nDigite o novo nome do cliente: ")
+                    clientes[email].update({'nome': novoNome})
+                    print('Nome atualizado.')
+                elif (escolha == 2):
+                    novoTelefone = input("Digite o novo telefone do cliente: ")
+                    clientes[email].update({'telefone': novoTelefone})
+                    print("Telefone atualizado")
+                elif (escolha == 3):
+                    continue
+                else:
+                    print("Opção Inválida.")
             else:
-                print("Opção Inválida.")
-        else:
-            print("\nCPF não encontrado.")
+                print("\nCPF não encontrado.")
 
     elif (decisao == 4):
-        cpf = str(input("\nDigite o CPF do cliente para removê-lo: "))
-        resultado = clientes.get(cpf, False)
+        email = str(input("\nDigite o E-mail do cliente para removê-lo: "))
+        resultado = clientes.get(email, False)
         if resultado:
-            del clientes[cpf]
+            del clientes[email]
             print("\nCliente removido.")
         else:
-            print("\nCPF não encontrado.")
+            print("\nE-mail não encontrado.")
 
     else:
         print("\nOpção inválida.")
